b5807881947f9d380930d378dc9cd1329a03dda5
Raúl García-Verdugo Cardador <pqe0023@gmail.com>
2020-10-09 12:11:06 +0200

    Pujada del tema 1

--- src/Ex1_v2.java
+++ src/Ex1_v2.java
@@ -12,14 +12,13 @@ public class Ex1_v2 {
          *
          * @param n1, n2, n3 seràn de tipus int
          * @param sc serà de tipus Scanner per emmagatzemar els números
-         * @return retornam una llista
+         * @output retornam una llista empleant Collections per mostrar una ArrayList de Integers
          *
          **/
 
         Scanner sc = new Scanner(System.in);
         int n1, n2, n3;
         List<Integer> ordered = new ArrayList<Integer>();
-        int comptador = 0;
 
         // Demanam a l'usuari per cli els números
         System.out.println("Digues un número:");
@@ -31,11 +30,21 @@ public class Ex1_v2 {
         System.out.println("Digues un número:");
         n3 = sc.nextInt();
 
+        // Afegim els nombres a la llista
         ordered.add(n1);
         ordered.add(n2);
         ordered.add(n3);
+        // Ordenam la llista fent servir un objecte Collections
         Collections.sort(ordered);
 
+        // Feim un bucle per a llistar els nombres ordenats pel seu valor
+        /**
+         *
+         *  Emplearem un bucle for on i serà un valor int que començarà en 0 i que anirà incrementant-se en funció
+         *  del valor de la longitud de la llista que li passem ens anirà imprimint per pantalla els números ordenats
+         *  pel seu valor
+         *
+         * */
         for(int i : ordered){
             System.out.println(i);
         }
