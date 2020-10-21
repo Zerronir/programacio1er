26e3651ec22b362a3cca93e7196fb39173bdc284
Raúl García-Verdugo Cardador <pqe0023@gmail.com>
2020-10-21 16:24:58 +0200

    Ex 3 y 4

---
+++ src/Ex3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Ex3 {
+    public static void main(String[] args) {
+
+        int times, cont;
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Cuantas veces quieres sumar?");
+
+
+    }
+}
